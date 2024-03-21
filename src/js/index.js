0c341251509999ce8e8c1a4cc99138bfb0204a75
AlessioBucheri <157613253+AlessioBucheri@users.noreply.github.com>
2024-03-21 01:35:12 +0100

    Configurazione base webpack

--- src/js/index.js
+++
@@ -1,4 +0,0 @@
-import Note from './note.js';
-import '../css/style.css';
-
-const nota1 =new Note();
--- webpack.config.js
+++ webpack.config.js
@@ -1,34 +1,63 @@
+// Generated using webpack-cli https://github.com/webpack/webpack-cli
+
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
-module.exports = {
-    entry: {
-        index: './src/js/index.js'
-    },
+const isProduction = process.env.NODE_ENV == 'production';
+
+
+const stylesHandler = isProduction ? MiniCssExtractPlugin.loader : 'style-loader';
+
+
+
+const config = {
+    entry: './src/index.js',
     output: {
         path: path.resolve(__dirname, 'dist'),
-        filename: 'bundle.js'
     },
-    module: {
-        rules: [
-            {
-                test: /\.css$/i,
-                use: [
-                    'style-loader',
-                    'css-loader']
-            }
-        ]
+    devServer: {
+        open: true,
+        host: 'localhost',
     },
     plugins: [
         new HtmlWebpackPlugin({
-            title: 'Applicazione Webpack',
-            template: './src/index.html',
-        })
+            template: 'index.html',
+        }),
+
+        // Add your plugins here
+        // Learn more about plugins from https://webpack.js.org/configuration/plugins/
     ],
-    devServer: {
-        port: 5000, 
-        open: true,
-        static: path.resolve(__dirname, 'dist'),
+    module: {
+        rules: [
+            {
+                test: /\.(js|jsx)$/i,
+                loader: 'babel-loader',
+            },
+            {
+                test: /\.css$/i,
+                use: [stylesHandler,'css-loader'],
+            },
+            {
+                test: /\.(eot|svg|ttf|woff|woff2|png|jpg|gif)$/i,
+                type: 'asset',
+            },
+
+            // Add your rules for custom modules here
+            // Learn more about loaders from https://webpack.js.org/loaders/
+        ],
     },
-    mode: 'development',
-}
+};
+
+module.exports = () => {
+    if (isProduction) {
+        config.mode = 'production';
+        
+        config.plugins.push(new MiniCssExtractPlugin());
+        
+        
+    } else {
+        config.mode = 'development';
+    }
+    return config;
+};
