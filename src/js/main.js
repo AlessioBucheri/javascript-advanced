96160b23931915eeb0f3d0bd39f274de646b7b4b
AlessioBucheri <157613253+AlessioBucheri@users.noreply.github.com>
2024-04-02 19:32:37 +0200

    edit style

--- src/js/main.js
+++ src/js/main.js
@@ -26,7 +26,6 @@ function getNewsOnScreen(Array) {
   Array.forEach(item => {
     if (item.url) {
     const newsItem = document.createElement('div')
-    console.log(item.url);
     newsItem.id="news-item";
     newsItem.innerHTML = `<h3 id="news-title">${item.title}</h3>
     <a id="anchor" href="${item.url}" target="_blank">Click here to read</a>
